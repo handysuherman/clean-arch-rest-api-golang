67ebeabdaa540ba69421e7816b2c1efb4a36ab40
handysuherman <lireya95@gmail.com>
2024-12-03 20:04:03 +0800

    build: pkg.mysql and pkg.redis constructor methods

--- go.mod
+++ go.mod
@@ -5,17 +5,20 @@ go 1.23.3
 require (
 	github.com/cloudwego/hertz v0.9.3
 	github.com/go-playground/validator/v10 v10.23.0
+	github.com/go-sql-driver/mysql v1.8.1
 	github.com/google/uuid v1.4.0
 	github.com/oklog/ulid/v2 v2.1.0
 	github.com/opentracing/opentracing-go v1.2.0
 	github.com/pkg/errors v0.9.1
 	github.com/prometheus/client_golang v1.20.5
+	github.com/redis/go-redis/v9 v9.7.0
 	github.com/rs/zerolog v1.33.0
 	github.com/spf13/viper v1.19.0
 	github.com/uber/jaeger-client-go v2.30.0+incompatible
 )
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/HdrHistogram/hdrhistogram-go v1.1.2 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/bytedance/go-tagexpr/v2 v2.9.2 // indirect
@@ -25,6 +28,7 @@ require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
 	github.com/cloudwego/iasm v0.2.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
---
+++ internal/pkg/databases/mysql/mysql.go
@@ -0,0 +1,73 @@
+package MariaDB
+
+import (
+	"crypto/tls"
+	"database/sql"
+	"fmt"
+	"time"
+
+	"github.com/go-sql-driver/mysql"
+	// _ "github.com/go-sql-driver/mysql"
+)
+
+type Config struct {
+	Driver                string         `json:"driver"`
+	Source                string         `json:"source"`
+	MaxOpenConnection     *int           `json:"max_open_connection"`
+	MaxIdleConnection     *int           `json:"max_idle_connection"`
+	MaxConnectionIdleTime *time.Duration `json:"max_connection_idle_time"`
+	MaxConnectionLifeTime *time.Duration `json:"max_connection_life_time"`
+	TLStype               string         `json:"tlsType"`
+	TLSEnable             bool           `json:"tlsEnable"`
+	TLS                   *tls.Config    `json:"tls"`
+}
+
+const (
+	maxConn         = 50
+	maxConnIdleTime = 1 * time.Minute
+	maxConnLifeTime = 3 * time.Minute
+)
+
+func NewMariaDBConn(config *Config) (*sql.DB, error) {
+	db, err := sql.Open(config.Driver, config.Source)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open database connection: %v", err)
+	}
+
+	// Common settings for both TLS and non-TLS connections
+	db.SetMaxIdleConns(10)
+
+	if config.MaxIdleConnection != nil {
+		db.SetMaxIdleConns(*config.MaxIdleConnection)
+	}
+
+	db.SetMaxOpenConns(maxConn)
+
+	if config.MaxOpenConnection != nil {
+		db.SetMaxOpenConns(*config.MaxOpenConnection)
+	}
+
+	db.SetConnMaxIdleTime(maxConnIdleTime)
+
+	if config.MaxConnectionIdleTime != nil {
+		db.SetConnMaxIdleTime(*config.MaxConnectionIdleTime)
+	}
+
+	db.SetConnMaxLifetime(maxConnLifeTime)
+
+	if config.MaxConnectionLifeTime != nil {
+		db.SetConnMaxLifetime(*config.MaxConnectionLifeTime)
+	}
+
+	// TLS
+	if config.TLSEnable {
+		mysql.RegisterTLSConfig(config.TLStype, config.TLS)
+	}
+
+	if err := db.Ping(); err != nil {
+		db.Close()
+		return nil, fmt.Errorf("failed to ping database: %v", err)
+	}
+
+	return db, nil
+}
