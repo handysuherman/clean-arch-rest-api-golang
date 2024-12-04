436b40f66552f83dce1ddcd5d50e475c93bbab9a
handysuherman <lireya95@gmail.com>
2024-12-04 10:37:46 +0800

    build: graceful shutdown functionality

--- internal/app/utils.go
+++ internal/app/utils.go
@@ -18,6 +18,16 @@ func (a *app) shutdownProcess(ctx context.Context) error {
 		return err
 	}
 
+	if err := a.metricsServer.Shutdown(ctx); err != nil {
+		a.log.Infof("a.metricsServer.Shutdown.err: %v", err)
+		return err
+	}
+
+	if err := a.gracefulShutDownHealthCheckServer(ctx); err != nil {
+		a.log.Infof("a.gracefulShutDownHealthCheckServer.err: %v", err)
+		return err
+	}
+
 	<-a.doneCh
 	a.log.Info("server shutdown gracefully")
 	return nil
