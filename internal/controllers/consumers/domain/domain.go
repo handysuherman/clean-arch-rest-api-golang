5d5d8e5224fbed5e307ba4a7836ababab5cd4646
handysuherman <lireya95@gmail.com>
2024-12-04 21:47:56 +0800

    build: consumers
    - dto
    - usecase params
    - mapper
    - usecase interface

--- internal/controllers/consumers/domain/domain.go
+++ internal/controllers/consumers/domain/domain.go
@@ -1,3 +1,12 @@
 package domain
 
-type Usecase interface{}
+import (
+	"context"
+)
+
+type Usecase interface {
+	Create(ctx context.Context, arg *CreateRequestParams) (int, error)
+	Update(ctx context.Context, arg *UpdateRequestParams) error
+	FindByID(ctx context.Context, id int) (*Consumer, error)
+	List(ctx context.Context, arg *FetchParams) (*ConsumerList, error)
+}
---
+++ internal/controllers/consumers/domain/params.go
@@ -0,0 +1,30 @@
+package domain
+
+import "github.com/handysuherman/studi-kasus-pt-xyz-golang-developer/internal/pkg/helper"
+
+type CreateRequestParams struct {
+	Nik         string  `json:"nik" validate:"required,gte=0,lte=16"`
+	FullName    string  `json:"full_name" validate:"required,gte=5,lte=255"`
+	LegalName   string  `json:"legal_name"`
+	BirthPlace  string  `json:"birth_place"`
+	BirthDate   string  `json:"birth_date"`
+	Salary      float64 `json:"salary"`
+	KTPPhoto    string  `json:"ktp_photo"`
+	SelfiePhoto string  `json:"selfie_photo"`
+}
+
+type UpdateRequestParams struct {
+	Nik         *string  `json:"nik"`
+	FullName    *string  `json:"full_name"`
+	LegalName   *string  `json:"legal_name"`
+	BirthPlace  *string  `json:"birth_place"`
+	BirthDate   *string  `json:"birth_date"`
+	Salary      *float64 `json:"salary"`
+	KTPPhoto    *string  `json:"ktp_photo"`
+	SelfiePhoto *string  `json:"selfie_photo"`
+}
+
+type FetchParams struct {
+	SearchText string             `json:"search_text"`
+	Pagination *helper.Pagination `json:"pagination"`
+}
