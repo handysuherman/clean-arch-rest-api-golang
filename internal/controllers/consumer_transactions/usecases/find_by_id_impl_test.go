194de97230070f9570d24963f20135b12e968ee9
handysuherman <lireya95@gmail.com>
2024-12-05 12:42:34 +0800

    build: find_by_id statisfy tests

--- internal/controllers/consumer_transactions/usecases/find_by_id_impl_test.go
+++ internal/controllers/consumer_transactions/usecases/find_by_id_impl_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"strconv"
 	"testing"
 
 	"github.com/handysuherman/studi-kasus-pt-xyz-golang-developer/internal/controllers/consumer_transactions/domain"
@@ -25,9 +26,9 @@ func Test_MOCK_FIND_BY_ID(t *testing.T) {
 			tname: "OK_CACHE_HIT",
 			id:    mockArgs.repoResponse.ID,
 			stubs: func(store *mock.MockRepository) {
-				store.EXPECT().Get(gomock.Any(), gomock.Eq(mockArgs.repoResponse.ID)).Times(1).Return(mockArgs.repoResponse, nil)
+				store.EXPECT().Get(gomock.Any(), gomock.Eq(strconv.FormatInt(mockArgs.repoResponse.ID, 10))).Times(1).Return(mockArgs.repoResponse, nil)
 				store.EXPECT().FindByID(gomock.Any(), EqFindByIDMatcher(mockArgs.repoResponse.ID)).Times(0)
-				store.EXPECT().Put(gomock.Any(), gomock.Eq(mockArgs.repoResponse.ID), gomock.Eq(mockArgs.repoResponse)).Times(0)
+				store.EXPECT().Put(gomock.Any(), gomock.Eq(strconv.FormatInt(mockArgs.repoResponse.ID, 10)), gomock.Eq(mockArgs.repoResponse)).Times(0)
 			},
 			checkResponse: func(t *testing.T, res *domain.ConsumerTransaction, err error) {
 				require.NoError(t, err)
@@ -65,16 +66,21 @@ func Test_MOCK_FIND_BY_ID(t *testing.T) {
 				require.Equal(t, mockArgs.repoResponse.AffiliatedDealerID, res.AffiliatedDealerID)
 				require.Equal(t, mockArgs.repoResponse.CreatedAt, res.CreatedAt)
 				require.Equal(t, mockArgs.repoResponse.UpdatedAt, res.UpdatedAt)
-				require.Equal(t, mockArgs.repoResponse.UpdatedBy, res.UpdatedBy)
+
+				if mockArgs.repoResponse.UpdatedBy.Valid {
+					require.Equal(t, mockArgs.repoResponse.UpdatedBy.String, *res.UpdatedBy)
+				} else {
+					require.Equal(t, res.UpdatedBy, nil)
+				}
 			},
 		},
 		{
 			tname: "OK_CACHE_MISS",
 			id:    mockArgs.repoResponse.ID,
 			stubs: func(store *mock.MockRepository) {
-				store.EXPECT().Get(gomock.Any(), gomock.Eq(mockArgs.repoResponse.ID)).Times(1).Return(nil, errors.New("redis not found"))
+				store.EXPECT().Get(gomock.Any(), gomock.Eq(strconv.FormatInt(mockArgs.repoResponse.ID, 10))).Times(1).Return(nil, errors.New("redis not found"))
 				store.EXPECT().FindByID(gomock.Any(), EqFindByIDMatcher(mockArgs.repoResponse.ID)).Times(1).Return(mockArgs.repoResponse, nil)
-				store.EXPECT().Put(gomock.Any(), gomock.Eq(mockArgs.repoResponse.ID), gomock.Eq(mockArgs.repoResponse)).Times(1)
+				store.EXPECT().Put(gomock.Any(), gomock.Eq(strconv.FormatInt(mockArgs.repoResponse.ID, 10)), gomock.Eq(mockArgs.repoResponse)).Times(1)
 			},
 			checkResponse: func(t *testing.T, res *domain.ConsumerTransaction, err error) {
 				require.NoError(t, err)
@@ -112,16 +118,21 @@ func Test_MOCK_FIND_BY_ID(t *testing.T) {
 				require.Equal(t, mockArgs.repoResponse.AffiliatedDealerID, res.AffiliatedDealerID)
 				require.Equal(t, mockArgs.repoResponse.CreatedAt, res.CreatedAt)
 				require.Equal(t, mockArgs.repoResponse.UpdatedAt, res.UpdatedAt)
-				require.Equal(t, mockArgs.repoResponse.UpdatedBy, res.UpdatedBy)
+
+				if mockArgs.repoResponse.UpdatedBy.Valid {
+					require.Equal(t, mockArgs.repoResponse.UpdatedBy.String, *res.UpdatedBy)
+				} else {
+					require.Equal(t, res.UpdatedBy, nil)
+				}
 			},
 		},
 		{
 			tname: "ERR_FIND_BY_ID_NOT_FOUND",
 			id:    mockArgs.repoResponse.ID,
 			stubs: func(store *mock.MockRepository) {
-				store.EXPECT().Get(gomock.Any(), gomock.Eq(mockArgs.repoResponse.ID)).Times(1).Return(nil, errors.New("redis not found"))
+				store.EXPECT().Get(gomock.Any(), gomock.Eq(strconv.FormatInt(mockArgs.repoResponse.ID, 10))).Times(1).Return(nil, errors.New("redis not found"))
 				store.EXPECT().FindByID(gomock.Any(), EqFindByIDMatcher(mockArgs.repoResponse.ID)).Times(1).Return(nil, sql.ErrNoRows)
-				store.EXPECT().Put(gomock.Any(), gomock.Eq(mockArgs.repoResponse.ID), gomock.Eq(mockArgs.repoResponse)).Times(1)
+				store.EXPECT().Put(gomock.Any(), gomock.Eq(strconv.FormatInt(mockArgs.repoResponse.ID, 10)), gomock.Eq(mockArgs.repoResponse)).Times(0)
 			},
 			checkResponse: func(t *testing.T, res *domain.ConsumerTransaction, err error) {
 				require.Error(t, err)
@@ -132,9 +143,9 @@ func Test_MOCK_FIND_BY_ID(t *testing.T) {
 			tname: "ERR_FIND_BY_ID_INTERNAL_SERVER_ERROR",
 			id:    mockArgs.repoResponse.ID,
 			stubs: func(store *mock.MockRepository) {
-				store.EXPECT().Get(gomock.Any(), gomock.Eq(mockArgs.repoResponse.ID)).Times(1).Return(nil, errors.New("redis not found"))
+				store.EXPECT().Get(gomock.Any(), gomock.Eq(strconv.FormatInt(mockArgs.repoResponse.ID, 10))).Times(1).Return(nil, errors.New("redis not found"))
 				store.EXPECT().FindByID(gomock.Any(), EqFindByIDMatcher(mockArgs.repoResponse.ID)).Times(1).Return(nil, sql.ErrConnDone)
-				store.EXPECT().Put(gomock.Any(), gomock.Eq(mockArgs.repoResponse.ID), gomock.Eq(mockArgs.repoResponse)).Times(1)
+				store.EXPECT().Put(gomock.Any(), gomock.Eq(strconv.FormatInt(mockArgs.repoResponse.ID, 10)), gomock.Eq(mockArgs.repoResponse)).Times(0)
 			},
 			checkResponse: func(t *testing.T, res *domain.ConsumerTransaction, err error) {
 				require.Error(t, err)
