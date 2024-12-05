2dc49e05720b76dff4f554cdeaac6d31cb59e1de
handysuherman <lireya95@gmail.com>
2024-12-05 18:50:05 +0800

    build: affilated dealer update usecase

--- internal/controllers/affiliated_dealers/domain/domain.go
+++ internal/controllers/affiliated_dealers/domain/domain.go
@@ -4,7 +4,7 @@ import "context"
 
 type Usecase interface {
 	Create(ctx context.Context, arg *CreateRequestParams) (int64, error)
-	Update(ctx context.Context, id int64, arg *UpdateRequestParams) error
+	Update(ctx context.Context, id int64, arg *UpdateRequestParams) (int64, error)
 	FindByID(ctx context.Context, id int64) (*AffiliatedDealer, error)
 	List(ctx context.Context, arg *FetchParams) (*AffiliatedDealerList, error)
 }
