9a154650cb88160326c78765222ba812b71c5cd7
handysuherman <lireya95@gmail.com>
2024-12-05 09:21:50 +0800

    refactor: adapt consumer transactions usecase interface with idempotency key occurence

--- internal/controllers/consumer_transactions/domain/domain.go
+++ internal/controllers/consumer_transactions/domain/domain.go
@@ -3,8 +3,8 @@ package domain
 import "context"
 
 type Usecase interface {
-	Create(ctx context.Context, arg *CreateRequestParams) (int, error)
-	Update(ctx context.Context, id int, arg *UpdateRequestParams) (int, error)
+	Create(ctx context.Context, arg *CreateRequestParams, idempotencyKey *string) (int, error)
+	Update(ctx context.Context, id int, arg *UpdateRequestParams, idempotencyKey *string) (int, error)
 	FindByID(ctx context.Context, id int) (*ConsumerTransaction, error)
 	List(ctx context.Context, arg *FetchParams) (*ConsumerTransactionList, error)
 }
